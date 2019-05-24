84ad229504d7728daf4b4334d26a94e1169dede4
HOJA MUSTAFFA ABDUL LATHEEF <hoja.ma@pitsolutions.com>
2019-05-24 14:32:23 +0530

    Backend Hook Initialized for loading Angular assets

---
+++ Classes/Hook/BackendHook.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Snowflake\Snowbabel\Hook;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2018 Anu Bhuvanendran Nair <anu.bn@pitsolutions.com>, PIT Solutions
+ *
+ *  You may not remove or change the name of the author above. See:
+ *  http://www.gnu.org/licenses/gpl-faq.html#IWantCredit
+ *
+ *  This script is part of the Typo3 project. The Typo3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class BackendHook
+{
+
+    /**
+     * @var string
+     */
+    public $queryBuilder = null;
+
+    /**
+     * Description
+     * @return type
+     */
+    public function __construct()
+    {
+        $this->queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class);
+    }
+
+    /**
+     * Execute PreRenderHook for possible manipulation:
+     * Add deepl.css,overrides localization.js and recordlist.js
+     */
+    public function executePreRenderHook(&$hook)
+    {
+        //override Localization.js
+        $flag = 0;
+
+        if (is_array($hook['cssFiles'])){
+            foreach ($hook['cssFiles'] as $key => $value) {
+                if (is_int(strpos($key,"angular-material.min.css"))){
+                    $flag = 1;
+                    break;
+                }
+            }
+        }
+        
+        if ($flag == 1){
+            unset($hook['cssFiles']['sysext/backend/Resources/Public/Css/backend.css']);
+        }        
+
+    }
+}
